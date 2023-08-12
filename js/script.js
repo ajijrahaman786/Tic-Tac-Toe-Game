ff7d5a0ada40ffed8e54e1840dfa8c5784709dbe
Ajij Rahaman <ajij@appycodes.com>
2023-08-12 13:23:58 +0530

    tic tac toe

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-let div1Btn = document.getElementById("div1");
-let div2Btn = document.getElementById("div2");
-let div3Btn = document.getElementById("div3");
-let div4Btn = document.getElementById("div4");
-let div5Btn = document.getElementById("div5");
-let div6Btn = document.getElementById("div6");
-let div7Btn = document.getElementById("div7");
-let div8Btn = document.getElementById("div8");
-let div9Btn = document.getElementById("div9");
+// let div1Btn = document.getElementById("div1");
+// let div2Btn = document.getElementById("div2");
+// let div3Btn = document.getElementById("div3");
+// let div4Btn = document.getElementById("div4");
+// let div5Btn = document.getElementById("div5");
+// let div6Btn = document.getElementById("div6");
+// let div7Btn = document.getElementById("div7");
+// let div8Btn = document.getElementById("div8");
+// let div9Btn = document.getElementById("div9");
 
 
