90bb4ffd8c4dab8376a31cd69bf6f3b9dabcb686
Ajij Rahaman <ajij@appycodes.com>
2023-08-17 15:55:40 +0530

    tic tac toe

--- js/script.js
+++ js/script.js
@@ -1,11 +1,66 @@
-// let div1Btn = document.getElementById("div1");
-// let div2Btn = document.getElementById("div2");
-// let div3Btn = document.getElementById("div3");
-// let div4Btn = document.getElementById("div4");
-// let div5Btn = document.getElementById("div5");
-// let div6Btn = document.getElementById("div6");
-// let div7Btn = document.getElementById("div7");
-// let div8Btn = document.getElementById("div8");
-// let div9Btn = document.getElementById("div9");
+ // set default turn
+ let currentTurn = '0';
+ // set flag for when wins X or 0 after then others boxes disable
+ let flag=false;
+ // wins message for winner
+ let winsMessage = document.querySelector(".wins");
+  // get in in the boxes and adding the event
+ let clickBtn = document.querySelectorAll('.click-btn');
+ // run loop for each boxes
+ clickBtn.forEach(function(btn, index){
+     btn.addEventListener("click", function(){
+         currentTurn == "0"?currentTurn = "X":currentTurn = "0";
+
+         if(flag){
+             return;
+         }
+
+         if(btn.innerHTML == ""){
+             btn.innerHTML = currentTurn;
+         }
+
+         // check winner
+         if(checkWinner()){
+             // alert("YAYA");
+             flag=true;
+             winsMessage.innerHTML = "Wins! "+ currentTurn;
+             document.getElementById('winingImg').src = "images/wins.gif";
+         }
+
+     });
+ });
 
 
+
+ // checking X or 0 are same line
+ let divs = document.querySelector(".btn-wrapper").children;
+ function checkWinner(){
+         const winningCombos = [
+         [0,3,6],[1,4,7],[2,5,8],
+         [0,1,2],[3,4,5],[6,7,8],
+         [0,4,8],[2,4,6]
+         ]
+
+         for(let i = 0; i<winningCombos.length; i++){
+
+             let [a,b,c] = winningCombos[i];
+
+             if((divs[a].innerHTML && divs[b].innerHTML && divs[c].innerHTML) && (divs[a].innerHTML==divs[b].innerHTML && divs[a].innerHTML == divs[c].innerHTML)){
+
+                return true;
+             }
+         }
+         return false;
+
+     }
+
+
+
+ // reset button
+
+ let resetBtn = document.getElementById("resetBtn");
+
+ resetBtn.addEventListener("click", function(){
+     location.reload();
+ });
+ 
