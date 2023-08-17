b46ab55e2df2ecb9f7f897e33b659d5661787b60
Ajij Rahaman <ajij@appycodes.com>
2023-08-17 17:11:41 +0530

    tic tac toe

--- js/script.js
+++ js/script.js
@@ -1,30 +1,32 @@
- // set default turn
- let currentTurn = '0';
- // set flag for when wins X or 0 after then others boxes disable
- let flag=false;
- // wins message for winner
+ // Initialize game variables
+ let currentTurn = '0';  // 'X' or '0'
+ let flag=false; // Indicates if the game is over
  let winsMessage = document.querySelector(".wins");
-  // get in in the boxes and adding the event
  let clickBtn = document.querySelectorAll('.click-btn');
- // run loop for each boxes
+// Add click event listeners to each box
  clickBtn.forEach(function(btn, index){
      btn.addEventListener("click", function(){
+        // Toggle the turn between 'X' and '0'
          currentTurn == "0"?currentTurn = "X":currentTurn = "0";
 
+         // If the game is over, return
          if(flag){
              return;
          }
 
+        //  set the currentTurn in the boxes
          if(btn.innerHTML == ""){
-             btn.innerHTML = currentTurn;
+            btn.innerHTML = currentTurn;
          }
 
-         // check winner
+         // Check for a winner
          if(checkWinner()){
-             // alert("YAYA");
-             flag=true;
-             winsMessage.innerHTML = "Wins! "+ currentTurn;
+             flag = true;
+             winsMessage.innerHTML = currentTurn + " is Wins!";
              document.getElementById('winingImg').src = "images/wins.gif";
+            //  btn.style.backgroundColor = "aquamarine";
+         }else if(checkdraw()){
+            winsMessage.innerHTML = "Match is Draw!";
          }
 
      });
@@ -32,9 +34,10 @@
 
 
 
- // checking X or 0 are same line
+ // Function to check if there's a winner
  let divs = document.querySelector(".btn-wrapper").children;
  function checkWinner(){
+        // Define winning combinations
          const winningCombos = [
          [0,3,6],[1,4,7],[2,5,8],
          [0,1,2],[3,4,5],[6,7,8],
@@ -46,7 +49,6 @@
              let [a,b,c] = winningCombos[i];
 
              if((divs[a].innerHTML && divs[b].innerHTML && divs[c].innerHTML) && (divs[a].innerHTML==divs[b].innerHTML && divs[a].innerHTML == divs[c].innerHTML)){
-
                 return true;
              }
          }
@@ -54,10 +56,19 @@
 
      }
 
+     // Function to check for a draw
+     function checkdraw(){
+        for (let i = 0; i < divs.length; i++) {
+          if(divs[i].innerHTML === ""){
+            return false; // At least one box is empty, not a draw
+          }
+            
+        }
+        return true; // All boxes are filled, it's a draw
+     }
 
 
- // reset button
-
+// Add click event listener to the reset button
  let resetBtn = document.getElementById("resetBtn");
 
  resetBtn.addEventListener("click", function(){
